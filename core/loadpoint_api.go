d26af3e77733974519d6c5ab97881f8029d7f4d3
andig <cpuidle@gmx.de>
2022-01-29 18:18:49 +0100

    Use nil as empty message value

--- core/loadpoint_api.go
+++ core/loadpoint_api.go
@@ -217,7 +217,12 @@ func (lp *LoadPoint) setRemainingDuration(chargeRemainingDuration time.Duration)
 
 	if lp.chargeRemainingDuration != chargeRemainingDuration {
 		lp.chargeRemainingDuration = chargeRemainingDuration
-		lp.publish("chargeRemainingDuration", chargeRemainingDuration)
+
+		if chargeRemainingDuration < 0 {
+			lp.publish("chargeRemainingDuration", nil)
+		} else {
+			lp.publish("chargeRemainingDuration", chargeRemainingDuration)
+		}
 	}
 }
 
